b6d92f1665bffefa5280c294690c7066f949d8fe
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2017-10-31 17:25:23 +0330

    Add JS Folder

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+// JavaScript Testing by MohamadKh75
+// 2017-07-19
+// ******************** 
+
+
+function makeItWhite()
+{
+    document.getElementById('my_img').src='img/white.png';
+}
+
+
+function makeItGray()
+{
+    document.getElementById('my_img').src='img/gray.png';
+}
+
+
+function makeItSmall()
+{
+    document.getElementById('my_img').style.width='100px';
+}
+
+
+function makeItBig()
+{
+    document.getElementById('my_img').style.width='500px';
+}
+
+
+function makeItNormal()
+{
+    document.getElementById('my_img').style.width='300px';
+}
+
+
+function HideIt()
+{
+    document.getElementById('my_img').style.display='none';
+}
+
+
+function ShowIt()
+{
+    document.getElementById('my_img').style.display='block';
+}
+
+
